1c57dce57daff24f71d2bb407300369baa3467c4
Preston nguyen <prestonnguyen12333@gmail.com>
2023-12-18 04:57:03 +0000

    describe the work you have completed

---
+++ Studies/variable.js
@@ -0,0 +1,29 @@
+/**
+ * Variables
+ * 
+ * 
+ * 0: variable are containers for data in JavaScript. We can create variables using declarative keywords...Variables are named identifiers that point to values like string, 
+ * object, arrays, numbers, boolean, or another datatype. Variables can also be reassign.
+ * 
+ * 1: declartion and Assignment: to create variables in JavaScript, we must first declare it by using one of three keywords.
+ * 
+ * 2: var, let, and const: var, let, and const are the three declaritive keywords.
+ * 
+ * 3: Hoisting: 
+ */
+
+// 1. declartion and assignments
+
+// declartion
+// undefined because cloths is not assigned
+var cloths;
+
+// assignment
+// cloths is nopw assign
+cloths = "hoodie";
+
+//2: var, let, and const
+
+// var
+
+var age;
